d399480c2778068a2ad66dad5827d893f1ff5547
khomah8 <59750654+khomah8@users.noreply.github.com>
2025-04-16 16:03:36 +0300

    Update PY_sc.py

--- PY_sc.py
+++ PY_sc.py
@@ -1,4 +1,7 @@
-# PY ground test 
+# PY ground test  
+## some useful URLs  
 - [Генератори в Python - aCode](https://acode.com.ua/generators-python/)
 - https://book.pythontips.com/en/latest/context_managers.html
+- https://www.w3schools.com/python/trypython.asp?filename=demo_iterator
+- https://www.devopsschool.com/blog/python-tutorials-difference-between-list-array-tuple-set-dict/
 - 
