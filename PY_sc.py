b34fa02fdf83d94ffb9343185c83c4c16ce781e7
khomah8 <59750654+khomah8@users.noreply.github.com>
2025-04-16 16:01:24 +0300

    Update PY_sc.py

--- PY_sc.py
+++ PY_sc.py
@@ -1,2 +1,4 @@
 # PY ground test 
-[Генератори в Python - aCode](https://acode.com.ua/generators-python/)
+- [Генератори в Python - aCode](https://acode.com.ua/generators-python/)
+- https://book.pythontips.com/en/latest/context_managers.html
+- 
